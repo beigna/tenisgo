a1243d7fc7059096d4d82fbea154d49836f62d11
Ignacio JM Benedetti <tranceway@gmail.com>
2023-03-14 13:08:41 -0300

    Agrega modelo Set

--- tenisgo/apps/matches/models.py
+++ tenisgo/apps/matches/models.py
@@ -14,11 +14,11 @@ class Match(models.Model):
     winner = models.ForeignKey('players.Player',
                                related_name='matches_won',
                                on_delete=models.CASCADE,
-                               blank=True)
+                               blank=True, null=True)
     loser = models.ForeignKey('players.Player',
                               related_name='matches_lost',
                               on_delete=models.CASCADE,
-                              blank=True)
+                              blank=True, null=True)
 
     def clean(self):
         if self.winner and self.looser:
@@ -32,3 +32,12 @@ class Match(models.Model):
 
             if self.loser not in (self.player_a, self.player_b):
                 raise ValidationError('El perdedor debe ser del partido')
+
+
+class Set(models.Model):
+    match = models.ForeignKey('matches.Match', on_delete=models.CASCADE)
+    order = models.SmalIntegerField(default=0)
+
+    is_tie_break = models.BooleanField(default=False)
+    score_a = models.SmalIntegerField(default=0)
+    score_b = models.SmalIntegerField(default=0)
